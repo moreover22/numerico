7a196d337f5ef8e024908c85c6d6c6df758457e2
otlucas <lucas.otero@despegar.com>
2019-10-05 21:44:06 -0300

    Fixed tf calculation
    Signed-off-by: otlucas <lucas.otero@despegar.com>

--- TP1/pythonCalculations.py
+++ TP1/pythonCalculations.py
@@ -1,15 +1,21 @@
 from busquedaRaices import secante, newton_raphson, punto_fijo, biseccion
 import matplotlib.pyplot as plt
+import math
 
 F = 900
 m = 475
-tf = 9
+
+A = 0
+B = 9
 
 xO = 0
 
-a = lambda t: (F / m) - 2 * (F / (m * tf)) * t
-v = lambda t: (F / m) * t - (F/(m * tf))* t ** 2
-x = lambda t: xO + (F/2*m) * t ** 2 - (F / (3 * m * tf)) * t ** 3
+tf = lambda t: math.sqrt((6 * (B - A) * m) / F)
+
+a = lambda t: (F / m) - 2 * (F / (m * tf(t))) * t
+v = lambda t: (F / m) * t - (F/(m * tf(t)))* t ** 2
+x = lambda t: xO + (F/2*m) * t ** 2 - (F / (3 * m * tf(t))) * t ** 3
+
 
 
 secs = [0, 0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8,8.5,9]
