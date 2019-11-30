b92a865bdcf7ab9ba61e664d54b35864726b7d62
otlucas <lucas.otero@despegar.com>
2019-11-30 11:27:18 -0300

    Modified printed string
    Signed-off-by: otlucas <lucas.otero@despegar.com>

--- TP2/grafico.py
+++ TP2/grafico.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from math import degrees
 
 def empezar_grafico():
-    plt.figure("Péndulo amortizado - TP2 numérico")
+    plt.figure("Péndulo amortiguado - TP2 numérico")
 
 def cargar_grafica(data, m, l, b, orden):
     tiempo = data[:, 0]
@@ -17,7 +17,7 @@ def cargar_grafica(data, m, l, b, orden):
 
     plt.xlabel('tiempo [s]')
     plt.ylabel(r'$\theta$ [deg] y $\dot{\theta}$ [deg/s]')
-    plt.title(f'Péndulo - masa = {m} long = {l} b = {b}')
+    plt.title(f'Péndulo - masa = {m} | long = {l} | amortiguación = {b}')
     plt.legend()
 
 def graficar():
